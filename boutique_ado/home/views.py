653080a523cb09f574dbeb661319dfc7cd34d428
Enquil <jim.e.olesen@gmail.com>
2023-04-06 12:21:34 +0000

    connect home app to allauth and boutique_ado

--- boutique_ado/home/views.py
+++ boutique_ado/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    '''
+    View that returns index.html
+    '''
+    return render(request, 'home/index.html')
